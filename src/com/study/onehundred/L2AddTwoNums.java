b6add8e2f519bc3e37772c25b8bbf7bdaca52f61
pyy <869849673@qq.com>
2020-03-16 15:44:16 +0800

    leetCode2

--- src/com/study/onehundred/L2AddTwoNums.java
+++ src/com/study/onehundred/L2AddTwoNums.java
@@ -16,15 +16,51 @@ Explanation: 342 + 465 = 807.
 * */
 public class L2AddTwoNums {
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return  null;
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode p1 = l1, p2 = l2;
+        int sum = 0;
+        ListNode cur = dummy;
+        while (p1 != null || p2 != null) {
+            if (p1 != null) {
+                sum += p1.val;
+                p1 = p1.next;
+            }
+            if (p2 != null) {
+                sum += p2.val;
+                p2 = p2.next;
+            }
+            cur.next = new ListNode(sum % 10);
+            sum /= 10;
+            cur = cur.next;
+        }
+        if (sum == 1){
+            cur.next = new ListNode(1);
+        }
+        return  dummy.next;
     }
 
     public static void main(String[] args) {
+        ListNode l1 = new ListNode(3);
+        ListNode l2 = new ListNode(4);
+        ListNode l3 = new ListNode(2);
+        l1.next = l2;
+        l2.next = l3;
 
+        ListNode r1 = new ListNode(4);
+        ListNode r2 = new ListNode(6);
+        ListNode r3 = new ListNode(5);
+        r1.next = r2;
+        r2.next = r3;
+
+        ListNode listNode = addTwoNumbers(l1, r1);
+        while (listNode != null) {
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
     }
 
-    class ListNode{
+    static  class ListNode{
         int val;
         ListNode next;
 
