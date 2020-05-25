edb2812ca790d36962ca5a03c13ba1ca12caa3b9
pangyangyang <869849673@qq.com>
2020-05-25 16:19:41 +0800

    leetcode21 dfs

---
+++ src/com/study/onehundred/L22GenerateParenthesis.java
@@ -0,0 +1,35 @@
+package com.study.onehundred;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author pangyangyang
+ * @Date 2020/05/25
+ **/
+public class L22GenerateParenthesis {
+
+  static List<String> res = new ArrayList<>();
+  public static List<String> generateParenthesis(int n) {
+    dfs(n, n, "");
+    return res;
+  }
+
+  private static void dfs(int left, int right, String s) {
+    if (left == 0 && right == 0) {
+      res.add(s);
+      return;
+    }
+    if (left > 0) {
+      dfs(left -1, right, s + "(");
+    }
+    if (right > left) {
+      dfs(left, right - 1, s+ ")");
+    }
+  }
+
+
+  public static void main(String[] args) {
+    System.out.println(generateParenthesis(3));
+  }
+}
